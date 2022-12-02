6b602c3f14808e908072fb071c07353417ac4e8e
ikkierie <ikkierie@protonmail.com>
2022-12-02 08:53:51 +0000

    removed duplicate solution from day 1 oneliner

--- 01/oneline.jl
+++ 01/oneline.jl
@@ -4,11 +4,3 @@ println("Part one: $(
         for x ∈ split(read("input.txt", String), r"(\r?\n){2}") 
     ]))[end]
 )\nPart two: $(sum(c[end-2:end]))")
-
-
-println("Part one: $(
-    (c = split.(split(read("input.txt", String), r"(\r?\n){2}"), r"(\r?\n)") .|>
-        (x -> sum(parse.(Int, x))) |> sort
-    )[end]
-)\nPart two: $(sum(c[end-2:end]))")
-
