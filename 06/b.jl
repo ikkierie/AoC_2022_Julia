f48a137ae3dd9c49e97e13154eb859f0471152de
ikkierie <ikkierie@protonmail.com>
2022-12-06 05:42:22 +0000

    added day 6 solutions

---
+++ 06/b.jl
@@ -0,0 +1,10 @@
+begin
+    local path  = raw"input.txt"
+    local input = readchomp(path)
+    
+    local n = 14
+    println(findfirst(
+        i -> length(∩(input[i-n+1:i])) == n, 
+        n:length(input)
+    ) + n - 1)
+end
