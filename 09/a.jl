df337ec9d3b7943f0138b853a9cd2339593e6e9d
ikkierie <ikkierie@protonmail.com>
2022-12-09 08:20:26 +0000

    added day 9 solutions

---
+++ 09/a.jl
@@ -0,0 +1,32 @@
+begin
+    local path  = raw"input.txt"
+    local input = match.(r"(\S) (\d+)", readlines(path))
+    
+    local head = (0, 0)
+    local tail = (0, 0)
+    
+    local grid = Set([tail])
+
+    local dirs = Dict(
+        "U" => ( 0,  1),
+        "D" => ( 0, -1),
+        "R" => ( 1,  0),
+        "L" => (-1,  0),
+    )
+
+    local safe = Set((x, y) for x ∈ -1:1, y ∈ -1:1)
+
+    for (dir, mag) ∈ enumerate(input)
+        for _ ∈ 1:parse(Int, mag)
+            head = head .+ dirs[dir]
+            if (local diff = tail .- head) ∉ safe
+                tail = !any(tail .== head) ?
+                    tail .+ copysign.((1, 1), (head .- tail))
+                    tail .+ (head .- tail) .- sign.(head .- tail)
+            end
+            push!(grid, tail)
+        end
+    end
+
+    println(length(grid))
+end
---
+++ 09/b.jl
@@ -0,0 +1,35 @@
+begin
+    local path  = raw"input.txt"
+    local input = match.(r"(\S) (\d+)", readlines(path))
+
+    local knots = [ (0, 0) for x ∈ 1:10 ]
+
+    local grid = Set(knots)
+
+    local dirs = Dict(
+        "U" => ( 0,  1),
+        "D" => ( 0, -1),
+        "R" => ( 1,  0),
+        "L" => (-1,  0),
+    )
+
+    local safe = Set((x, y) for x ∈ -1:1, y ∈ -1:1)
+
+    for (dir, mag) ∈ input
+        for _ ∈ 1:parse(Int, mag)
+            knots[1] = knots[1] .+ dirs[dir]
+            for k ∈ 2:10
+                local head = knots[k-1]
+                local tail = knots[k]
+                if (tail .- head) ∉ safe
+                    knots[k] = !any(tail .== head) ?
+                        tail .+ copysign.((1, 1), (head .- tail)) :
+                        tail .+ (head .- tail) .- sign.(head .- tail)
+                end
+            end
+            push!(grid, knots[end])
+        end
+    end
+
+    println(length(grid))
+end
