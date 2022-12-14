e27a1ed14c71918f60d9380afbb265c2d9dfe8bc
ikkierie <ikkierie@protonmail.com>
2022-12-14 19:32:05 +0000

    minor cleanup of day 11 solutions

--- 11/a.jl
+++ 11/a.jl
@@ -1,49 +1,45 @@
-begin
+test(n, a, b) = ((x) -> (x % n == 0) ? a + 1 : b + 1)
+    
+mutable struct Monkey
+    items::Vector{Int}
+    op::Function
+    test::Function
+    inspections::Int
+end
+
+function main()
     local path  = raw"input.txt"
-    local input = read(path, String)
-    
-    local test(n, a, b) = ((x) -> (x % n == 0) ? a + 1 : b + 1)
-    
-    mutable struct Monkey
-        items::Vector{Int}
-        op::Function
-        test::Function
-        inspections::Int
-    end
+    local input = replace(read(path, String), "\r\n" => "\n")
 
     local monkeys = Monkey[]
-    for (monkey, data) ∈ eachmatch(r"Monkey (\d+):\r\n([\s\S]+?)\r\n\r\n", input)
+    for (monkey, data) ∈ eachmatch(r"Monkey (\d+):\n(.+?)\n\n"s, input)
         push!(monkeys, Monkey(
+            # items list
             parse.(Int, split(
                 first(match(r"Starting items: (.+)", data)), 
                 r",\s*"
             )),
 
-            #= 
-                I'm pretty sure I'm doing something wrong here
-                with the metaprogramming here. Not sure how to
-                fix it/properly do what I want yet, though.
-            =#
-            eval(Expr(:->, 
-                :old, 
-                match(r"Operation: new = (.+)", data) |> 
-                    first                             |> 
-                    Meta.parse
-            )),
+            # operation function
+            "old -> $(first(match(r"Operation: new = (.+)", data)))" |> 
+                    Meta.parse |> eval,
 
+            # test function
             test(parse.(Int, collect(match(
-                r"divisible by (\d+)[\s\S]+?(\d+)[\s\S]+?(\d+)", 
+                r"divisible by (\d+).+?(\d+).+?(\d+)"s, 
                 data
             )))...),
 
+            # № inspections
             0
         ))
     end
 
     for round ∈ 1:20, monkey ∈ monkeys
-        while !isempty(monkey.items)
-            local worry = popfirst!(monkey.items)
-            worry = monkey.op(worry) ÷ 3
+        local items = monkey.items
+        while !isempty(items)
+            local worry = popfirst!(items)
+            worry = Base.invokelatest(monkey.op, worry) ÷ 3
             push!(monkeys[monkey.test(worry)].items, worry)
             monkey.inspections += 1
         end
@@ -53,3 +49,5 @@ begin
         prod                                          |>
         println
 end
+
+main()
--- 11/b.jl
+++ 11/b.jl
@@ -9,42 +9,40 @@ test(n, a, b) = ((x) -> (x % n == 0) ? a + 1 : b + 1)
 
 function main()
     local path  = raw"input.txt"
-    local input = read(path, String)
+    local input = replace(read(path, String), "\r\n" => "\n")
 
     local divisors = Int[]
     local monkeys = Monkey[]
-    for (monkey, data) ∈ eachmatch(r"Monkey (\d+):\r\n([\s\S]+?)\r\n\r\n", input)
+    for (monkey, data) ∈ eachmatch(r"Monkey (\d+):\n(.+?)\n\n"s, input)
         push!(monkeys, Monkey(
+            # items list
             parse.(Int, split(
                 first(match(r"Starting items: (.+)", data)), 
                 r",\s*"
             )),
 
-            eval(Expr(:->, 
-                :old, 
-                match(r"Operation: new = (.+)", data) |> 
-                    first                             |> 
-                    Meta.parse
-            )),
+            # operation function
+            "old -> $(first(match(r"Operation: new = (.+)", data)))" |> 
+                    Meta.parse |> eval,
 
+            # test function
             test(parse.(Int, collect(match(
-                r"divisible by (\d+)[\s\S]+?(\d+)[\s\S]+?(\d+)", 
+                r"divisible by (\d+).+?(\d+).+?(\d+)"s, 
                 data
             )))...),
 
+            # № inspections
             0
         ))
-        push!(
-            divisors, 
-            parse(Int, first(match(r"divisible by (\d+)", data)))
-        )
+        push!(divisors, parse(Int, first(match(r"divisible by (\d+)", data))))
     end
 
-    local common_div = lcm(prod(divisors))
+    local common_div = lcm(divisors)
 
     for round ∈ 1:10000, monkey ∈ monkeys
-        while !isempty(monkey.items)
-            local worry = popfirst!(monkey.items)
+        local items = monkey.items
+        while !isempty(items)
+            local worry = popfirst!(items)
             worry = Base.invokelatest(monkey.op, worry) % common_div
             push!(monkeys[monkey.test(worry)].items, worry)
             monkey.inspections += 1
